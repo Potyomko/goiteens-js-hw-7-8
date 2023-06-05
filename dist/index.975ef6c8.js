d46261299852299ceba30416a7ef5fe6accef018
Potyomko Veronika <vnpotomko@gmail.com>
2023-06-05 09:49:57 +0300

    Все працює

--- dist/index.975ef6c8.js
+++ dist/index.975ef6c8.js
@@ -568,31 +568,31 @@ var _brightThemeCss = require("@pnotify/core/dist/BrightTheme.css");
 (0, _core.defaultModules).set(_mobile, {});
 const inputEl = document.querySelector(".input");
 const listEl = document.querySelector(".list");
-inputEl.addEventListener("input", (0, _debounceDefault.default)(onSearch, 500));
-function onSearch(event) {
-    event.preventDefault();
-    console.log(event.target.value);
-    const countryName = event.target.value;
-    fetch(`https://restcountries.com/v3.1/name/${countryName}`).then((response)=>response.json()).then((data)=>{
+inputEl.addEventListener("input", (0, _debounceDefault.default)(onSearch, 1000));
+function onSearch(e) {
+    e.preventDefault();
+    console.log(e.target.value);
+    const nameOfCountry = e.target.value;
+    fetch(`https://restcountries.com/v3.1/name/${nameOfCountry}`).then((response)=>response.json()).then((data)=>{
         const countries = data.map((country)=>country.name.common);
-        const numCountries = countries.length;
-        if (numCountries > 1) {
+        const countCountries = countries.length;
+        if (countCountries > 1) {
             listEl.innerHTML = countries.map((name)=>`<p>${name}</p>`).join("");
             listEl.classList.add("list-1");
         }
-        if (numCountries === 1) {
+        if (countCountries === 1) {
             const country = data[0];
             listEl.classList.add("list-1");
             listEl.innerHTML = `
-        <img src="${country.flags.png}" alt="${country.name.common} flag">
-        <h1>${country.name.common}</h1>
-        <p>Capital: ${country.capital[0]}</p>
-        <p>Population: ${country.population}</p>
-        <p>Region: ${country.region}</p
+      <img src="${country.flags.png}" alt="${country.name.common} flag">
+        <h1>${data[0].name.common}</h1>
+        <p>Capital: ${data[0].capital}</p>
+        <p>Population: ${data[0].population}</p>
+        <p>Region: ${data[0].region}</p>
       `;
         }
     }).catch((error)=>{
-        (0, _core.alert)("❗️Ведіть країну❗️");
+        (0, _core.alert)("❗️Ведіть щось❗️");
     });
 }
 
--- src/index.js
+++ src/index.js
@@ -4,37 +4,41 @@ import '@pnotify/core/dist/PNotify.css';
 import * as PNotifyMobile from '@pnotify/mobile';
 import '@pnotify/mobile/dist/PNotifyMobile.css';
 import '@pnotify/core/dist/BrightTheme.css';
+
 defaultModules.set(PNotifyMobile, {});
- 
+
 const inputEl = document.querySelector('.input')
 const listEl = document.querySelector('.list')
 
-inputEl.addEventListener('input', debounce(onSearch, 500))
+inputEl.addEventListener('input', debounce(onSearch, 1000))
 
-function onSearch(event) {
-  event.preventDefault()
-  console.log(event.target.value)
-  const countryName = event.target.value
-  fetch(`https://restcountries.com/v3.1/name/${countryName}`)
+function onSearch(e) {
+  e.preventDefault()
+  console.log(e.target.value)
+  const nameOfCountry = e.target.value
+  
+  fetch(`https://restcountries.com/v3.1/name/${nameOfCountry}`)
   .then(response => response.json())
   .then(data => {
     const countries = data.map(country => country.name.common);
-    const numCountries = countries.length;
-    if (numCountries > 1) {
+    const countCountries = countries.length;
+
+    if (countCountries > 1) {
       listEl.innerHTML = countries.map(name => `<p>${name}</p>`).join('');
       listEl.classList.add('list-1')
     }
-    if (numCountries === 1) {
+    
+    if (countCountries === 1) {
       const country = data[0];
       listEl.classList.add('list-1')
       listEl.innerHTML = `
-        <img src="${country.flags.png}" alt="${country.name.common} flag">
-        <h1>${country.name.common}</h1>
-        <p>Capital: ${country.capital[0]}</p>
-        <p>Population: ${country.population}</p>
-        <p>Region: ${country.region}</p
+      <img src="${country.flags.png}" alt="${country.name.common} flag">
+        <h1>${data[0].name.common}</h1>
+        <p>Capital: ${data[0].capital}</p>
+        <p>Population: ${data[0].population}</p>
+        <p>Region: ${data[0].region}</p>
       `;
     }
   })
-.catch(error=> { alert("❗️Ведіть країну❗️")} )
+.catch(error=> { alert("❗️Ведіть щось❗️")} )
 }
